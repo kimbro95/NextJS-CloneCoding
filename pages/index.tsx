bebb1aacbbe1c2e7532ce7e09081abd56f4e6406
l995lol4 <gudgudgud714@naver.com>
2022-03-08 19:21:05 +0900

    #4.4 Modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,9 +17,12 @@ const Home: NextPage = () => {
           <span className='font-semibold'>Total</span>
           <span className='font-semibold'>$40</span>
         </div>
-        <div className='bg-blue-500 text-center text-white rounded-lg mt-4 mx-auto w-3/4 py-3'>
+        <button 
+        className='bg-blue-500 display: block text-white rounded-lg mt-4 mx-auto w-3/4 py-3 
+        hover:bg-teal-500 hover:text-black
+        active:bg-yellow-500 focus:text-red-500'>
           Checkout
-        </div>
+        </button>
       </div>
       <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
         <div className="bg-blue-500 p-6 pb-14">
@@ -57,10 +60,10 @@ const Home: NextPage = () => {
           <span className="font-medium text-xl">Swoon Lounge</span>
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-0 mb-5 flex justify-between items-center">
-            <div>
-              <input type="radio" />
-              <input type="radio" />
-              <input type="radio" />
+            <div className='space-x-2'>
+              <button className='w-5 h-5 rounded-full bg-yellow-500'></button>
+              <button className='w-5 h-5 rounded-full bg-blue-500'></button>
+              <button className='w-5 h-5 rounded-full bg-green-500'></button>
             </div>
             <div className="flex items-center space-x-5">
               <button className=" rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
