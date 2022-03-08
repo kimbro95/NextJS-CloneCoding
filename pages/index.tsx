5b7239568120368f6469a33a619c6cfb0ad4c5c7
l995lol4 <gudgudgud714@naver.com>
2022-03-08 19:44:49 +0900

    #4.5 Transitions ( Ring )

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,9 +61,9 @@ const Home: NextPage = () => {
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-0 mb-5 flex justify-between items-center">
             <div className='space-x-2'>
-              <button className='w-5 h-5 rounded-full bg-yellow-500'></button>
-              <button className='w-5 h-5 rounded-full bg-blue-500'></button>
-              <button className='w-5 h-5 rounded-full bg-green-500'></button>
+              <button className='w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition'></button>
+              <button className='w-5 h-5 rounded-full bg-blue-500 focus:ring-2 ring-offset-2 ring-blue-500 transition'></button>
+              <button className='w-5 h-5 rounded-full bg-green-500 focus:ring-2 ring-offset-2 ring-green-500 transition'></button>
             </div>
             <div className="flex items-center space-x-5">
               <button className=" rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
