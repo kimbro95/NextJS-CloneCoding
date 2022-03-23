c2e88b9b40c01f4c3dc47b5623e6ae316fb7e56e
bro <gudgudgud714@naver.com>
2022-03-23 14:14:51 +0900

    #7.4 Validation part Two

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,33 +1,43 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface FormType {
+    username: string;
+    email: string;
+    password: string;
+}
 
 export default function Forms() {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = () => {
+    const { register, handleSubmit } = useForm<FormType>();
+    const onSubmit = (data: FormType) => {
         console.log("Hi onSubmit");
     };
-    const onError = () => {
-        console.log("Hi onError");
+    const onError = (err: FieldErrors) => {
+        console.log(err);
     }
 
     return (
         <form onSubmit={handleSubmit(onSubmit, onError)}>
             <input
                 {...register("username", {
-                    required: true,
+                    required: "Username is required",
+                    minLength: {
+                        message: "5자 이하임",
+                        value: 5,
+                    }
                 })}
                 type="text"
                 placeholder="UserName"
             />
             <input
                 {...register("email", {
-                    required: true,
+                    required: "Email is required",
                 })}
                 type="email"
                 placeholder="Email"
             />
             <input
                 {...register("password", {
-                    required: true,
+                    required: "Password is required",
                 })}
                 type="password"
                 placeholder="Password"
