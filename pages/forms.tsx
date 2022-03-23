29773139cd9845b316671cb5212d593233dd02c1
bro <gudgudgud714@naver.com>
2022-03-23 14:55:40 +0900

    #7.5 Errors

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -7,14 +7,16 @@ interface FormType {
 }
 
 export default function Forms() {
-    const { register, handleSubmit } = useForm<FormType>();
+    const { register, handleSubmit, formState: { errors } } = useForm<FormType>({
+        mode: "onChange",
+    });
     const onSubmit = (data: FormType) => {
         console.log("Hi onSubmit");
     };
     const onError = (err: FieldErrors) => {
-        console.log(err);
+        //console.log(err);
     }
-
+    console.log(errors);
     return (
         <form onSubmit={handleSubmit(onSubmit, onError)}>
             <input
@@ -31,10 +33,14 @@ export default function Forms() {
             <input
                 {...register("email", {
                     required: "Email is required",
+                    validate: {
+                        notGmail: (value) => !value.includes("@gmail.com") ? "" : "Gmail is not allowed",
+                    }
                 })}
                 type="email"
                 placeholder="Email"
             />
+            {errors.email?.message}
             <input
                 {...register("password", {
                     required: "Password is required",
@@ -42,6 +48,7 @@ export default function Forms() {
                 type="password"
                 placeholder="Password"
             />
+            <br />
             <input type="submit" value="Create" />
         </form>
     )
