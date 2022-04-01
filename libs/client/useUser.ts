83c4c60925274507c13b9bce0f2e688815ebf4f5
l995lol4 <gudgudgud714@naver.com>
2022-04-01 23:23:48 +0900

    10.2 useUser Hook

---
+++ libs/client/useUser.ts
@@ -0,0 +1,17 @@
+import { route } from "next/dist/server/router";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function useUser(){
+    const router = useRouter();
+    const [user, setUser] = useState();
+    useEffect(() => {
+        fetch("/api/users/me")
+        .then(response => response.json())
+        .then((data) => {
+            if(!data.ok) return router.replace("/enter");
+            setUser(data.profile);
+        });
+    }, [router]);
+    return user;
+}
