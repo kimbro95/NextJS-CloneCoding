8731d5e056f7ff5af2c33ede67536f8e71992503
l995lol4 <gudgudgud714@naver.com>
2022-04-03 21:00:33 +0900

    10.4 useUser Refactor

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -2,12 +2,17 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const fetcher = ( url : string ) => fetch(url).then((response) => response.json());
-
 export default function useUser(){
-    const { data, error } = useSWR("/api/users/me", fetcher);
+    const { data, error } = useSWR("/api/users/me");
     const router = useRouter();
-    //return router.replace("/enter");
+    useEffect(() => {
+        if(data && !data.ok){
+            router.replace("/enter");
+        }
+    },[ data, router ]);
 
-    return data;
+    return {
+        user: data?.profile, 
+        isLoading : !data && !error 
+    };
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,40 +5,42 @@ import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
 
 const Home: NextPage = () => {
-  const user = useUser();
-  console.log(user);
+  const { user, isLoading } = useUser();
+  console.log(user, isLoading);
   return (
-    <Layout title="홈" hasTabBar>
-      <div className="flex flex-col space-y-4 divide-y-[1px]">
-        {[...Array(20)].map((_, i) => (
-          <Item
-            id={i}
-            key={i}
-            title="iPhone 14"
-            price={99}
-            comments={1}
-            hearts={1}
-          />
-        ))}
-        <FloatingButton href="/items/upload">
-          <svg
-            className="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+    user ?
+      <Layout title="홈" hasTabBar>
+        <div className="flex flex-col space-y-4 divide-y-[1px]">
+          {[...Array(20)].map((_, i) => (
+            <Item
+              id={i}
+              key={i}
+              title="iPhone 14"
+              price={99}
+              comments={1}
+              hearts={1}
             />
-          </svg>
-        </FloatingButton>
-      </div>
-    </Layout>
+          ))}
+          <FloatingButton href="/items/upload">
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+          </FloatingButton>
+        </div>
+      </Layout>
+    : null
   );
 };
 
