b34ede0c4c3be11899929328fc12169bebab89d8
bro <gudgudgud714@naver.com>
2022-04-04 15:42:32 +0900

    #11.3 See Products

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -5,8 +5,10 @@ export interface ResponseType {
     [key: string]: any;
 }
 
+type method = "GET" | "POST" | "DELETE";
+
 interface ConfigType {
-    method: "GET" | "POST" | "DELETE",
+    methods: method[],
     handler: (
         req: NextApiRequest,
         res: NextApiResponse,
@@ -14,16 +16,16 @@ interface ConfigType {
     isPrivate?: boolean
 }
 
-export default function withHandler({method, handler, isPrivate = true} : ConfigType) {
+export default function withHandler({ methods, handler, isPrivate = true }: ConfigType) {
     return async function (
         req: NextApiRequest,
         res: NextApiResponse
     ): Promise<any> {
-        if (req.method !== method) {
+        if (req.method && !methods.includes(req.method as any)) {
             return res.status(405).end();
         }
-        if(isPrivate && !req.session.user?.id){
-            res.status(401).json({ ok : false });
+        if (isPrivate && !req.session.user?.id) {
+            res.status(401).json({ ok: false });
         }
         try {
             await handler(req, res);
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,27 @@ import FloatingButton from "@components/floating-button";
 import Item from "@components/item";
 import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
+import useSWR from "swr";
+import { Product } from "@prisma/client";
+
+interface ProductsResponse {
+  ok: boolean;
+  products: Product[];
+}
 
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
-  console.log(user, isLoading);
+  const { data } = useSWR<ProductsResponse>("/api/products");
   return (
     user ?
       <Layout title="홈" hasTabBar>
         <div className="flex flex-col space-y-4 divide-y-[1px]">
-          {[...Array(20)].map((_, i) => (
+          {data?.products?.map((product) => (
             <Item
-              id={i}
-              key={i}
-              title="iPhone 14"
-              price={99}
+              id={product.id}
+              key={product.id}
+              title={product.name}
+              price={product.price}
               comments={1}
               hearts={1}
             />
