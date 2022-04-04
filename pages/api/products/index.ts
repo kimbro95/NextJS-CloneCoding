0b65853be7543d290de326ffaa6f320927e2dd9f
bro <gudgudgud714@naver.com>
2022-04-04 15:28:22 +0900

    #11.2 Upload API

---
+++ pages/api/products/index.ts
@@ -0,0 +1,39 @@
+import { withApiSession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+
+async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseType>,
+) {
+    const {
+        body: { name, price, description },
+        session: { user },
+    } = req;
+    const product = await client.product.create({
+        data: {
+            name,
+            price: +price,
+            description,
+            image: "abc",
+            user: {
+                connect: {
+                    id: user?.id,
+                },
+            },
+        },
+    });
+    res.json({
+        ok: true,
+        product,
+    });
+}
+
+export default withApiSession(
+    withHandler({
+        method: "POST",
+        handler,
+        isPrivate: false,
+    })
+);
