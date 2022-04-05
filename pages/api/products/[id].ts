419da9da23078387f8238e3627bf4fe9c045312b
bro <gudgudgud714@naver.com>
2022-04-05 15:01:04 +0900

    11.5 Related Products

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -8,9 +8,10 @@ async function handler(
     res: NextApiResponse<ResponseType>,
 ) {
     const { id } = req.query;
+    const cleandId = +id.toString();
     const product = await client.product.findUnique({
         where: {
-            id: +id.toString(),
+            id: cleandId,
         },
         include: {
             user: {
@@ -22,10 +23,29 @@ async function handler(
             },
         },
     });
-    console.log(product);
+    const terms = product?.name.split(" ").map((word) => ({
+        name: {
+            contains: word,
+        },
+    }));
+    const relatedProducts = await client.product.findMany({
+        where: {
+            OR: terms,
+            AND: {
+                id: {
+                    not: cleandId,
+                },
+            },
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+        take: 4,
+    });
     res.json({
         ok: true,
         product,
+        relatedProducts,
     });
 }
 
