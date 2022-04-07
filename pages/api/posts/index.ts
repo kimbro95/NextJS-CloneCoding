a3f0fd391d9ecad83a35ee181784ee4ca76d5adf
bro <gudgudgud714@naver.com>
2022-04-07 11:13:18 +0900

    12.1 Forms and Handlers

---
+++ pages/api/posts/index.ts
@@ -0,0 +1,37 @@
+import { withApiSession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+
+async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseType>,
+) {
+    const {
+        body: { question },
+        session: { user },
+    } = req;
+
+    const post = await client.post.create({
+        data: {
+            question,
+            user: {
+                connect: {
+                    id: user?.id,
+                },
+            },
+        },
+    });
+    res.json({
+        ok: true,
+        post,
+    });
+}
+
+export default withApiSession(
+    withHandler({
+        methods: ["POST"],
+        handler,
+        isPrivate: false,
+    })
+);
--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -2,16 +2,47 @@ import { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
+import { Post } from "@prisma/client";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+interface WriteForm {
+    question: string;
+}
+
+interface WriteResponse {
+    ok: boolean;
+    post: Post;
+}
 
 const Write: NextPage = () => {
+    const router = useRouter();
+    const { register, handleSubmit } = useForm<WriteForm>();
+    const [post, { loading, data }] = useMutation<WriteResponse>("/api/posts");
+    const onValid = (data: WriteForm) => {
+        if (loading) return;
+        post(data);
+    }
+    useEffect(() => {
+        if (data && data.ok) {
+            router.push(`/community/${data.post.id}`);
+        }
+    }, [data, router]);
+
     return (
         <Layout canGoBack title="Write Post">
-            <form className="px-4 space-y-2">
+            <form onSubmit={handleSubmit(onValid)} className="px-4 py-2 space-y-2">
                 <TextArea
+                    register={register("question", {
+                        required: true,
+                        minLength: 5
+                    })}
                     required
                     placeholder="Ask a question!"
                 />
-                <Button text="Submit" />
+                <Button text={loading ? "Loading..." : "Submit"} />
             </form>
         </Layout>
     );
