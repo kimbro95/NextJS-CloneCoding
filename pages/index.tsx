3f83827bd30298c889953dd76963591345533be4
l995lol4 <gudgudgud714@naver.com>
2022-05-02 20:58:47 +0900

    19.7 SSR + SWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import FloatingButton from "@components/floating-button";
 import Item from "@components/item";
 import Layout from "@components/layout";
-import useSWRInfinite from "swr/infinite";
+import useSWRInfinite, { unstable_serialize } from "swr/infinite";
 import { Product } from "@prisma/client";
 import { useInfiniteScroll } from "@libs/client/useInfiniteScroll";
 import { useEffect } from "react";
+import { SWRConfig } from "swr";
+import client from "@libs/server/client";
 
 export interface ProductWitCount extends Product {
   _count: {
@@ -24,16 +26,19 @@ const getKey = (pageIndex: number, previousPageData: ProductsResponse) => {
   if (pageIndex + 1 > previousPageData.pages) return null;
   return `/api/products?page=${pageIndex + 1}`;
 };
-
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home: NextPage = () => {
+  // useSWRInfinite 사용법
+  // https://swr.vercel.app/ko/docs/pagination#useswrinfinite
   const { data, setSize } = useSWRInfinite<ProductsResponse>(getKey, fetcher);
   const products = data ? data.map((item) => item.products).flat() : [];
   const page = useInfiniteScroll();
+
   useEffect(() => {
     setSize(page);
   }, [setSize, page]);
+
   return (
       <Layout title="홈" hasTabBar seoTitle="Products">
         <div className="flex flex-col space-y-4 divide-y-[1px]">
@@ -44,7 +49,7 @@ const Home: NextPage = () => {
               title={product.name}
               price={product.price}
               comments={0}
-              hearts={product._count.favs}
+              hearts={product._count?.favs || 0}
               productImage={product.image}
             />
           ))}
@@ -70,4 +75,33 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+const Page: NextPage<ProductsResponse> = ({ products, pages }) => {
+  // unstable_serialize 사용
+  // https://github.com/vercel/swr/issues/1520#issuecomment-933247768
+    return(
+      <SWRConfig
+        value={{
+          fallback: {
+            [unstable_serialize(getKey)]: [{
+              ok: true,
+              products,
+              pages,
+            },],
+          },
+        }}
+      >
+        <Home />
+      </SWRConfig>
+    )
+};
+
+export const getServerSideProps: GetServerSideProps = async() =>{
+  const products = await client.product.findMany({});
+  return{
+    props:{
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
+}
+
+export default Page;
