ed9ea7c8af83e3151fecb5a1ab95e435599bd2b3
bro <gudgudgud714@naver.com>
2022-05-06 16:24:33 +0900

    19.13 Dynamic getStaticProps

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,13 +1,18 @@
 import { readdirSync } from "fs";
-import { NextPage } from "next";
+import matter from "gray-matter";
+import { NextPage, GetServerSideProps } from "next";
+import remarkHtml from "remark-html";
+import remarkParse from "remark-parse";
+import { validateRequest } from "twilio/lib/webhooks/webhooks";
+import { unified } from "unified";
 
-const Post: NextPage = () => {
+const Post: NextPage<{ post: string }> = ({ post }) => {
     return (
-        <h1>111</h1>
+        <h1>{post}</h1>
     );
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths = ({ }) => {
     const files = readdirSync("./posts").map((file) => {
         const [name, _] = file.split(".");
         return { params: { slug: name } };
@@ -18,10 +23,15 @@ export const getStaticPaths = () => {
     };
 }
 
-export const getStaticProps = () => {
+export const getStaticProps: GetServerSideProps = async (ctx) => {
+    const { content } = matter.read(`./posts/${ctx.params?.slug}.md`);
+    const { value } = await unified()
+        .use(remarkParse)
+        .use(remarkHtml)
+        .process(content);
     return {
         props: {
-
+            post: value,
         },
     };
 }
