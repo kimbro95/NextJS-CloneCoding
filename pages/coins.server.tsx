a6835553b6f73e5ed5f37b26963c5e81d72701e1
bro <gudgudgud714@naver.com>
2022-05-18 14:55:24 +0900

    21.4 Parallelism

--- pages/coins.server.tsx
+++ pages/coins.server.tsx
@@ -3,24 +3,42 @@ import { Suspense } from "react";
 const cache: any = {};
 function fetchData(url: string) {
     if (!cache[url]) {
-        throw fetch(url)
-            .then(res => res.json())
-            .then(json => cache[url] = json.slice(0, 20));
+        throw Promise.all([
+            fetch(url)
+                .then((res) => res.json())
+                .then((json) => (cache[url] = json)),
+            new Promise((resolve) =>
+                setTimeout(resolve, Math.round(Math.random() * 5000))
+            ),
+        ]);
     }
     return cache[url];
 }
 
+function Coin({ id, name, symbol }: any) {
+    const { quotes: { USD: { price } } } = fetchData(`https://api.coinpaprika.com/v1/tickers/${id}`);
+    return (
+        <span key={id}>
+            {name} | {symbol} | ${price}
+        </span>
+    )
+}
+
 function List() {
     const coins = fetchData("https://api.coinpaprika.com/v1/coins");
-    //console.log(coins);
     return (
-        <ul>
-            {coins.map((coin: any) => (
-                <li key={coin.id}>
-                    {coin.name} | {coin.symbol}
-                </li>
-            ))}
-        </ul>
+        <div>
+            <h4>Coin List</h4>
+            <ul>
+                {coins.slice(0, 20).map((coin: any) => (
+                    <li key={coin.id}>
+                        <Suspense fallback={`Loading ${coin.name}...`}>
+                            <Coin {...coin} />
+                        </Suspense>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
